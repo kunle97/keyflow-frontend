e857652133771a2cef6d6587b7305e78c4855413
kunle <adekunle.ashton@gmail.com>
2023-08-28 06:47:31 -0400

    fixed accept reject rentall  applciation feature

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -21,3 +21,15 @@ export function stringToBoolean(string) {
       return Boolean(string);
   }
 }
+
+export function createApprovalHash(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
