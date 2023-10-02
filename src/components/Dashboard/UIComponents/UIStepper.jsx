b6d802f00300288a16aa8225e113867907c35d4f
kunle <adekunle.ashton@gmail.com>
2023-10-02 08:32:24 -0400

    feat:KFLW-20 added steps to the rental application

---
+++ src/components/Dashboard/UIComponents/UIStepper.jsx
@@ -0,0 +1,129 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import { styled } from "@mui/material/styles";
+import Stack from "@mui/material/Stack";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import Check from "@mui/icons-material/Check";
+import StepConnector, {
+  stepConnectorClasses,
+} from "@mui/material/StepConnector";
+import { uiGreen } from "../../../constants";
+import { withStyles } from "@mui/styles";
+import { Typography } from "@mui/material";
+
+const QontoConnector = styled(StepConnector)(({ theme }) => ({
+  [`&.${stepConnectorClasses.alternativeLabel}`]: {
+    top: 10,
+    left: "calc(-50% + 16px)",
+    right: "calc(50% + 16px)",
+  },
+  [`&.${stepConnectorClasses.active}`]: {
+    [`& .${stepConnectorClasses.line}`]: {
+      borderColor: uiGreen,
+    },
+  },
+  [`&.${stepConnectorClasses.completed}`]: {
+    [`& .${stepConnectorClasses.line}`]: {
+      borderColor: uiGreen,
+    },
+  },
+  [`& .${stepConnectorClasses.line}`]: {
+    borderColor:
+      theme.palette.mode === "dark" ? theme.palette.grey[800] : "#eaeaf0",
+    borderTopWidth: 3,
+    borderRadius: 1,
+  },
+}));
+
+const QontoStepIconRoot = styled("div")(({ theme, ownerState }) => ({
+  color: "white",
+  display: "flex",
+  height: 22,
+  alignItems: "center",
+  ...(ownerState.active && {
+    color: uiGreen,
+  }),
+  "& .QontoStepIcon-completedIcon": {
+    color: uiGreen,
+    zIndex: 1,
+    fontSize: 18,
+  },
+  "& .QontoStepIcon-circle": {
+    width: 8,
+    height: 8,
+    borderRadius: "50%",
+    backgroundColor: "currentColor",
+  },
+}));
+
+function QontoStepIcon(props) {
+  const { active, completed, className } = props;
+
+  return (
+    <QontoStepIconRoot ownerState={{ active }} className={className}>
+      {completed ? (
+        <Check className="QontoStepIcon-completedIcon" />
+      ) : (
+        <div className="QontoStepIcon-circle" />
+      )}
+    </QontoStepIconRoot>
+  );
+}
+
+QontoStepIcon.propTypes = {
+  /**
+   * Whether this step is active.
+   * @default false
+   */
+  active: PropTypes.bool,
+  className: PropTypes.string,
+  /**
+   * Mark the step as completed. Is passed to child components.
+   * @default false
+   */
+  completed: PropTypes.bool,
+};
+
+const steps = [
+  "Basic Information",
+  "Additional Information",
+  "Employment History",
+  "Residence History",
+  "Submit Application",
+];
+const styles = {
+  // Define your custom styles here
+  whiteLabel: {
+    color: "white", // Change this to your desired label color
+  },
+};
+
+function UIStepper(props) {
+
+
+  return (
+    <Stack sx={{ width: "100%" }} spacing={4}>
+      <Stepper
+        alternativeLabel
+        activeStep={props.step}
+        connector={<QontoConnector />}
+      >
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel
+              StepIconComponent={QontoStepIcon}
+              // className={classes.whiteLabel}
+            >
+              <Typography className={props.classes.whiteLabel}>
+                {label}
+              </Typography>
+            </StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+    </Stack>
+  );
+}
+export default withStyles(styles)(UIStepper);
---
+++ src/components/RentalApplication/ApplicationSections/RentalHistorySection.jsx
@@ -0,0 +1,210 @@
+import Checkbox from "@mui/material/Checkbox";
+import React from "react";
+import { uiGreen, validationMessageStyle } from "../../../constants";
+import UIButton from "../../Dashboard/UIComponents/UIButton";
+import { Button, Stack } from "@mui/material";
+
+const RentalHistorySection = (props) => {
+  const {
+    address,
+    startDate,
+    endDate,
+    landlordName,
+    landlordPhone,
+    landlordEmail,
+  } = props.residence;
+
+  return (
+    <>
+      <div className="card mb-3">
+        <div className="row card-body">
+          <div className="col-md-12 mb-4">
+            <label className="mb-2">
+              Full Address (Street, City, State, Zip)
+            </label>
+            <input
+              {...props.register(`address_${props.id}`, {
+                required: "This is a required field",
+              })}
+              className="form-control"
+              name="address"
+              defaultValue={address}
+              onChange={props.onResidenceChange}
+              sx={{ color: "white", width: "100%" }}
+              placeholder=" Address"
+            />
+            <span style={validationMessageStyle}>
+              {props.addressErrors && props.addressErrors.message}
+            </span>
+          </div>
+          <div className="col-md-6 mb-4">
+            <label className="mb-2">Start Date</label>
+            <input
+              {...props.register(`residenceStartDate_${props.id}`, {
+                required: "This is a required field",
+              })}
+              type="date"
+              className="form-control"
+              defaultValue={startDate}
+              onChange={props.onResidenceChange}
+              sx={{ color: "white", width: "100%" }}
+              placeholder="Start Date"
+            />
+            <span style={validationMessageStyle}>
+              {props.residenceStartDateErrors &&
+                props.residenceStartDateErrors.message}
+            </span>
+          </div>
+          <div className="col-md-6 mb-4">
+            <label className="mb-2">End Date</label>
+            <input
+              {...props.register(`residenceEndDate_${props.id}`, {
+                required: "This is a required field",
+                validate: (value) => {
+                  if (
+                    props.watch(`residenceStartDate_${props.id}`) > value ||
+                    value === "" ||
+                    value === props.watch(`residenceStartDate_${props.id}`)
+                  ) {
+                    return "End date must be after start date";
+                  }
+                },
+              })}
+              type="date"
+              className="form-control"
+              name="endDate"
+              defaultValue={endDate}
+              onChange={props.onResidenceChange}
+              sx={{ color: "white", width: "100%" }}
+              placeholder="End Date"
+            />
+            <Checkbox /> Current Residence
+            <div style={validationMessageStyle}>
+              {props.residenceEndDateErrors &&
+                props.residenceEndDateErrors.message}
+            </div>
+          </div>
+          <div className="col-md-12 mb-4">
+            <label className="mb-2">Landlord Name</label>
+            <input
+              {...props.register(`landlordName_${props.id}`, {
+                required: "This is a required field",
+              })}
+              className="form-control"
+              name="landlordName"
+              defaultValue={landlordName}
+              onChange={props.onResidenceChange}
+              sx={{ color: "white", width: "100%" }}
+              placeholder="Company Name"
+            />
+            <span style={validationMessageStyle}>
+              {props.landlordNameErrors && props.landlordNameErrors.message}
+            </span>
+          </div>
+          <div className="col-md-6 mb-4">
+            <label className="mb-2">Landlord Phone</label>
+            <input
+              {...props.register(`landlordPhone_${props.id}`, {
+                required: "This is a required field",
+                pattern: {
+                  value: /\d{3}-\d{3}-\d{4}/,
+                  message: "Please enter a valid phone number",
+                },
+              })}
+              className="form-control"
+              name="landlordPhone"
+              defaultValue={landlordPhone}
+              onChange={props.onResidenceChange}
+              sx={{ color: "white", width: "100%" }}
+              placeholder="Landlord Phone"
+            />
+            <span style={validationMessageStyle}>
+              {props.landlordPhoneErrors && props.landlordPhoneErrors.message}
+            </span>
+          </div>
+          <div className="col-md-6 mb-4">
+            <label className="mb-2">Landlord Email</label>
+            <input
+              {...props.register(`landlordEmail_${props.id}`, {
+                required: "This is a required field",
+                pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: "Please enter a valid email address",
+                },
+              })}
+              className="form-control"
+              name="landlordEmail"
+              defaultValue={landlordEmail}
+              onChange={props.onResidenceChange}
+              sx={{ color: "white", width: "100%" }}
+              placeholder="Landlord Email"
+            />
+            <span style={validationMessageStyle}>
+              {props.landlordEmailErrors && props.landlordEmailErrors.message}
+            </span>
+          </div>
+          <Stack direction="row" gap={2}>
+            {props.removeBtn}{" "}
+            {props.showStepButtons && (
+              <Button
+                sx={{
+                  background: uiGreen,
+                  textTransform: "none",
+                }}
+                variant="contained"
+                onClick={props.addRentalHistoryNode}
+              >
+                Add
+              </Button>
+            )}
+          </Stack>
+        </div>
+      </div>
+      {props.showStepButtons && (
+        <>
+          <Stack sx={{ marginTop: "20px" }} direction="row" gap={2}>
+            <UIButton
+              style={{ width: "100%" }}
+              btnText="Back"
+              onClick={props.previousStep}
+              type="button"
+            />
+            <UIButton
+              style={{ width: "100%" }}
+              btnText="Next"
+              onClick={() => {
+                props.trigger([
+                  `address_${props.id}`,
+                  `residenceStartDate_${props.id}`,
+                  `residenceEndDate_${props.id}`,
+                  `landlordName_${props.id}`,
+                  `landlordPhone_${props.id}`,
+                  `landlordEmail_${props.id}`,
+                ]);
+                if (
+                  props.addressErrors ||
+                  props.residenceStartDateErrors ||
+                  props.residenceEndDateErrors ||
+                  props.landlordNameErrors ||
+                  props.landlordPhoneErrors ||
+                  props.landlordEmailErrors
+                ) {
+                  props.setIsValid(false);
+                } else {
+                  props.setIsValid(true);
+                }
+
+                if (props.isValid) {
+                  props.nextStep();
+                }
+              }}
+              type="button"
+            />
+          </Stack>
+        </>
+      )}
+    </>
+  );
+};
+
+export default RentalHistorySection;
